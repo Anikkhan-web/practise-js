63c2f3b270d75e27e4f4e2a74dc175a88aff16f8
Anikkhan-web <anikkhan9521@gmail.com>
2021-08-04 16:21:32 -0700

    new commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function anik(n){
+    return n*2;
+}
+var yes = anik(10);
+console.log(yes);
+
+
+let name = "anik khan";
+
+for (let i = 1; i<=4; i++){
+    console.log(i);
+
+}
+let i = 0;
+while(i<=4){
+    console.log(i);
+    i++
+}
