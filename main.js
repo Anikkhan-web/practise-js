afd31661dc26a4d34ce1c9cbcf9e8bca4f53ea73
Anikkhan-web <anikkhan9521@gmail.com>
2021-08-05 11:21:09 -0700

    simple interest

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
-function anik(n){
-    return n*2;
+// function anik(n){
+//     return n*2;
+// }
+// var yes = anik(10);
+// console.log(yes);
+
+
+// let name = "anik khan";
+
+// for (let i = 1; i<=4; i++){
+//     console.log(i);
+
+// }
+// let i = 0;
+// while(i<=4){
+//     console.log(i);
+//     i++
+// }
+
+function simpleInterest(principleBalence){
+    var rateOfInterest = 7;
+    var time = 5;
+    var finalAmount = principleBalence*rateOfInterest*time/100;
+
+    var total = (principleBalence+finalAmount);
+    console.log(total);
+    return finalAmount;
+    return total;
 }
-var yes = anik(10);
-console.log(yes);
-
-
-let name = "anik khan";
-
-for (let i = 1; i<=4; i++){
-    console.log(i);
-
-}
-let i = 0;
-while(i<=4){
-    console.log(i);
-    i++
-}
+var result = simpleInterest(20000);
+console.log(result);
+// console.log(total);
