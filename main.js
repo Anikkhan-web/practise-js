816bc7b970ca05d7176381ef877818079f72a28f
Anikkhan-web <anikkhan9521@gmail.com>
2021-08-05 11:43:58 -0700

    grade calculator

--- main.js
+++ main.js
@@ -17,16 +17,47 @@
 //     i++
 // }
 
-function simpleInterest(principleBalence){
-    var rateOfInterest = 7;
-    var time = 5;
-    var finalAmount = principleBalence*rateOfInterest*time/100;
+// function simpleInterest(principleBalence){
+//     var rateOfInterest = 14;
+//     var time = 1;
+//     var finalAmount = principleBalence*rateOfInterest*time/(100*12);
 
-    var total = (principleBalence+finalAmount);
-    console.log(total);
-    return finalAmount;
-    return total;
+//     let total = (principleBalence+finalAmount);
+//     // return total;
+//     console.log(total);
+//     return finalAmount;
+
+    
+// }
+// var result = simpleInterest(50000);
+// console.log(result);
+// // console.log(total);
+
+
+var number =11;
+
+
+if(number>=80 && number <=100){
+    console.log("A+");
+
+}else if(number>=70 && number <=79){
+    console.log("A-");
+
+}else if(number>=60 && number <=69){
+    console.log("A");
+
+}else if(number>=50 && number <=59){
+    console.log("B");
+
+}else if(number>=40 && number <=49){
+    console.log("C");
+
+}else if(number>=33 && number <=39){
+    console.log("D");
+
+}else if(number>=1 && number<=32){
+    console.log("Fail")
 }
-var result = simpleInterest(20000);
-console.log(result);
-// console.log(total);
+else{
+    console.log("Not a valid number")
+}
