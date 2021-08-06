efb82895152f6f4a5c6a9be6b70d9779f214777d
Anikkhan-web <anikkhan9521@gmail.com>
2021-08-05 21:51:06 -0700

    result function

--- main.js
+++ main.js
@@ -36,42 +36,63 @@
 
 
 
-// function grade(number){
+function grade(number){
 
 
 
-// if(number>=80 && number <=100){
-//     console.log("A+");
+if(number>=80 && number <=100){
+    return (" A+");
 
-// }else if(number>=70 && number <=79){
-//     console.log("A-");
+}else if(number>=70 && number <=79){
+    return ("A-");
 
-// }else if(number>=60 && number <=69){
-//     console.log("A");
+}else if(number>=60 && number <=69){
+    return ("A");
 
-// }else if(number>=50 && number <=59){
-//     console.log("B");
+}else if(number>=50 && number <=59){
+    return ("B");
 
-// }else if(number>=40 && number <=49){
-//     console.log("C");
+}else if(number>=40 && number <=49){
+    return ("C");
 
-// }else if(number>=33 && number <=39){
-//     console.log("D");
+}else if(number>=33 && number <=39){
+    return ("D");
 
-// }else if(number>=1 && number<=32){
-//     console.log("Fail")
-// }
-// else{
-//     console.log("Not a valid number")
-// }
-// }
-// var result = grade(80);
-
-// console.log(result); 
-
-function totalHour(minutes){
-    var hours = minutes/60;
-    return hours;
+}else if(number>=1 && number<=32){
+    return ("Fail");
 }
-var modulesHour = totalHour(2372).toFixed(2);
-console.log(modulesHour);
+else{
+    return ("Not a valid number")
+}
+}
+var result = grade(40);
+console.log('your result is',result);
+ 
+
+// function totalHour(minutes){
+//    let hours = minutes/60;
+//     return hours;
+// }
+// const modulesHour = totalHour(2372).toFixed(2);
+// console.log('total modules hour',modulesHour);
+
+
+// items = ["anik","khan","kmn","asw",5,6];
+
+
+// for (var i = 0; i<= items.length; i++){
+
+//     console.log(items[i]);
+// }
+// year%4==0 && year%100!= 0 || year%400==0 leap year
+
+
+
+
+
+// for(var i = 11; i <= 30; i += 2)
+// {
+//  var sum = sum += i;
+//  console.log(sum)
+// }
+
