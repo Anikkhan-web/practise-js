0ea624c3441054ce9de96c49a95388ec2a45d6c7
Anikkhan-web <anikkhan9521@gmail.com>
2021-08-05 23:16:16 -0700

    compare number

--- main.js
+++ main.js
@@ -49,15 +49,39 @@
 //         console.log("Wrong Number");
 // }
 
-const number = 2.7;
-const output = Math.abs(number);
-const output = Math.random()*3
-const rounded = Math.floor(output)
+// const number = 2.7;
+// const output = Math.abs(number);
+// const output = Math.random()*3
+// const rounded = Math.floor(output)
 
-console.log(rounded);
-for (let i = 0; i<=10; i++){
-    const output = Math.random()*6
-const rounded = Math.round(output)
+// console.log(rounded);
+// for (let i = 0; i<=10; i++){
+//     const output = Math.random()*6
+// const rounded = Math.round(output)
 
-console.log(rounded);
+// console.log(rounded);
+// }
+
+// var first = 5;
+// var second = 7;
+// console.log(first,second); 
+// var tem = first; 
+ // first = second; 
+ // second = tem;
+ // console.log(first,second);
+
+// [first,second]=[second,first]
+// console.log(first,second);
+
+
+
+const b = 450;
+const m = 350;
+const a= 900
+if (b>m && b>a){
+    console.log("bbb");
+}else if(m>b && m>A){
+    console.log("mmmmm");
+}else{
+    console.log("a");
 }
